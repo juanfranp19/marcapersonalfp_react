aac7cdadb6bea64d6760e1cbb1007b02e92f0e2f
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-02-16 07:51:27 +0100

    el logo de marca personal lleva a '/'

--- src/components/Cabecera/Cabecera.jsx
+++ src/components/Cabecera/Cabecera.jsx
@@ -1,16 +1,23 @@
 
 import TituloMP from "../TituloMP/TituloMP";
 import Imagen from "../Imagen/Imagen";
+import {useNavigate} from "react-router-dom";
 
 const Cabecera = () => {
+
+  const navegateTo = useNavigate();
+
+  function aHome() {
+    navegateTo('/');
+  }
 
   return (
     <nav className="row">
       <div className="col-12 cabecera navbar navbar-expand-md">
           <div className="container-fluid">
-            <a className="navbar-brand" href="javascript:void(0)">
+            <div className="navbar-brand" onClick={aHome}>
               <Imagen clase='logoMP' cual='logoMP'></Imagen>
-            </a>
+            </div>
             <button className="navbar-toggler btn btn-seconday btn-lg" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
               <span className="navbar-toggler-icon"></span>
             </button>
