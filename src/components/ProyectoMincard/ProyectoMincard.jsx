645879f1d784f0291745e941816e89d98362bdff
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-02-20 19:09:33 +0100

    Estructura de la minCard de proyectos hecha

--- src/components/ProyectoMincard/ProyectoMincard.jsx
+++ src/components/ProyectoMincard/ProyectoMincard.jsx
@@ -1,44 +1,35 @@
 const ProyectoMinCard = () => {
 
     return (
-        <section className="w-100 px-4 py-5" style="background-color: #9de2ff; border-radius: .5rem .5rem 0 0;">
-            <div className="row d-flex justify-content-center">
-                <div className="col col-md-9 col-lg-7 col-xl-6">
-                    <div className="card" style="border-radius: 15px;">
-                        <div className="card-body p-4">
-                            <div className="d-flex">
-                                <div className="flex-shrink-0">
-                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp" 
-                                        alt="Generic placeholder image" className="img-fluid" style="width: 180px; border-radius: 10px;"/>
-                                </div>
-                                <div className="flex-grow-1 ms-3">
-                                    <h5 className="mb-1">Danny McLoan</h5>
-                                    <p className="mb-2 pb-1">Senior Journalist</p>
-                                    <div className="d-flex justify-content-start rounded-3 p-2 mb-2 bg-body-tertiary">
-                                        <div>
-                                            <p className="small text-muted mb-1">Articles</p>
-                                            <p className="mb-0">41</p>
-                                        </div>
-                                        <div className="px-3">
-                                            <p className="small text-muted mb-1">Followers</p>
-                                            <p className="mb-0">976</p>
-                                        </div>
-                                        <div>
-                                            <p className="small text-muted mb-1">Rating</p>
-                                            <p className="mb-0">8.5</p>
-                                        </div>
-                                    </div>
-                                    <div className="d-flex pt-1">
-                                        <button type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-outline-primary me-1 flex-grow-1">Chat</button>
-                                        <button type="button" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary flex-grow-1">Follow</button>
-                                    </div>
-                                </div>
-                            </div>
+        <div className="row justify-content-center">
+            <div className="col-12 col-sm-6 col-lg-4 col-xl-3">
+                <div className="card m-2">
+
+                    <div className="row p-4 card-body">
+                        <div className="col-6 col-sm-12 col-md-6 col-lg-12 col-xxl-6">
+                            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp" alt="Proyecto" className="w-100" />
+                        </div>
+
+                        <div className="col-6 col-sm-12 col-md-6 col-lg-12 col-xxl-6">
+
+                            <h1>Diseño Gráfico</h1>
+
+                            <h2>ALUMNOS</h2>
+                            <p>Nombre Nombre</p>
+
+                            <p><span>Tutor:</span> 2</p>
+                            <p><span>Ciclos:</span> DAW | DAM</p>
+
+                        </div>
+
+                        <div className="col-12 boton-ver">
+                            <button type="button" className=" w-100 btn btn-outline-primary">Ver</button>
                         </div>
                     </div>
+                    
                 </div>
             </div>
-        </section>
+        </div>
     );
 }
 
--- src/pages/Empresa/Proyectos.jsx
+++ src/pages/Empresa/Proyectos.jsx
@@ -1,14 +1,15 @@
 // librerías
 import React from 'react';
 // componentes
-import MenuEmpresa from '../../components/MenuEmpresa/MenuEmpresa';
 import ListaFamiliasProfesionales from '../../components/ListaFamiliasProfesionales/ListaFamiliasProfesionales';
+import ProyectoMinCard from '../../components/ProyectoMincard/ProyectoMincard';
 
 const Proyectos = () => {
     return (
         <div className="row">
             <div className="col-12">
                 <ListaFamiliasProfesionales></ListaFamiliasProfesionales>
+                <ProyectoMinCard></ProyectoMinCard>
             </div>
         </div>
     );
